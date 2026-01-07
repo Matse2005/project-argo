1f7bf048f00f0bf6bb7eb4b337ed7df7f7c30095
MatseVH <49431416+Matse2005@users.noreply.github.com>
2026-01-07 17:27:34 +0100

    Init

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const env = process.env.ENV || "unknown";
+  res.writeHead(200);
+  res.end(`Hello from ${env}\n`);
+});
+
+if (require.main === module) {
+  server.listen(3000, () => {
+    console.log("Server running");
+  });
+}
+
+module.exports = server;
---
+++ test/app.test.js
@@ -0,0 +1,32 @@
+process.env.ENV = "test";
+
+const http = require("http");
+const server = require("../src/app");
+
+let listener;
+
+beforeAll((done) => {
+  listener = server.listen(0, done);
+});
+
+afterAll((done) => {
+  listener.close(done);
+});
+
+test("returns environment in response", (done) => {
+  const port = listener.address().port;
+
+  http.get(`http://localhost:${port}`, (res) => {
+    let data = "";
+
+    res.on("data", (chunk) => {
+      data += chunk;
+    });
+
+    res.on("end", () => {
+      expect(res.statusCode).toBe(200);
+      expect(data).toBe("Hello from test\n");
+      done();
+    });
+  });
+});
